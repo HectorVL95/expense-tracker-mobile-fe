b37fa064fabe5a008b12c98756a0fa7c39cf4517
Hector Valenzuela <francisco1495@gmail.com>
2025-12-12 10:10:47 -0700

    progress

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ export default function App() {
     <NavigationContainer>
 
       <Stack.Navigator>
-        
+        <Stack.Screen name='Home' component={}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
