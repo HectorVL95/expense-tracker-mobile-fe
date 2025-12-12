47fbfba8647ee2b1ddf89d817a7d49d5b2a9dea6
Hector Valenzuela <francisco1495@gmail.com>
2025-12-12 11:58:06 -0700

    progres

--- App.tsx
+++ App.tsx
@@ -2,15 +2,15 @@ import { View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import './global.css';
+import Login from 'app/screens/login';
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-
       <Stack.Navigator>
-        <Stack.Screen name='Home' component={}/>
+        <Stack.Screen name='Login' component={<Login/>}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ app/screens/login.tsx
@@ -0,0 +1,13 @@
+import {View, Text, TextInput} from 'react-native'
+
+const Login = () => {
+  return (
+    <View className=''>
+      <View>
+
+      </View>
+    </View>
+  );
+}
+
+export default Login;
